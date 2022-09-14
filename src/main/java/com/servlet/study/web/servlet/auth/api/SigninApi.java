43c7e56aeb7902a0a39df94289b15755b65d3bb5
code1218 <skjil1218@gmail.com>
2022-09-14 09:52:39 +0900

    로그인 세션 처리

--- src/main/java/com/servlet/study/web/servlet/auth/api/SigninApi.java
+++ src/main/java/com/servlet/study/web/servlet/auth/api/SigninApi.java
@@ -3,33 +3,53 @@ package com.servlet.study.web.servlet.auth.api;
 import java.io.IOException;
 import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.swing.text.AbstractDocument.Content;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.servlet.study.web.dto.auth.SignupRequestDto;
+import com.servlet.study.web.service.UserService;
+import com.servlet.study.web.servlet.dto.PrincipalUser;
 
 @WebServlet("/api/v1/auth/signin")
 public class SigninApi extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private UserService userService;
        
     public SigninApi() {
         super();
     }
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    	userService = (UserService) config.getServletContext().getAttribute("userService");
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+    	
+    	HttpSession session = request.getSession();
+    	
+    	PrincipalUser principalUser = (PrincipalUser) session.getAttribute("principal");
+    	
+    	response.setContentType("application/json; charset=utf-8");
+    	response.getWriter().print(gson.toJson(principalUser));
+    }
+    
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServletContext context = request.getServletContext();
-		
 		String userJson = request.getParameter("user");
-		
-		System.out.println(userJson);
 		
 		Gson jsonUser = new GsonBuilder().setPrettyPrinting().create();
 		
@@ -38,28 +58,23 @@ public class SigninApi extends HttpServlet {
 		String userId = jsonObject.get("userId").toString();
 		String userPassword = jsonObject.get("userPassword").toString();
 		
-		SignupRequestDto signupRequestDto = (SignupRequestDto) context.getAttribute("userData");
+		PrincipalUser principalUser = userService.signin(userId, userPassword);
 		
 		Gson responseData = new Gson();
 		JsonObject data = new JsonObject();
 		
-		if(signupRequestDto.getUserId().equals(userId)) {
-			if(signupRequestDto.getUserPassword().equals(userPassword)) {
-				System.out.println("로그인 인증 성공");
-				
-				data.addProperty("status", true);
-				
-				response.setContentType("application/json; charset=utf-8");
-				response.getWriter().print(responseData.toJson(data));
-				return;
-			}
+		if(principalUser == null) {
+			data.addProperty("status", false);
+		}else {
+			data.addProperty("status", true);
+			HttpSession session = request.getSession();
+			session.setAttribute("principal", principalUser);
+			session.setMaxInactiveInterval(60 * 10);
 		}
-		
-		data.addProperty("status", false);
 		
 		response.setContentType("application/json; charset=utf-8");
 		response.getWriter().print(responseData.toJson(data));
-		return;
+		
 	}
 
 }
